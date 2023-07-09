6e01b5422f10a1809e34ebfa98b06c2ae8ad7c24
Jacek <jckkupczyk@gmail.com>
2023-07-09 18:18:01 +0200

    Konfiguracja dostępu bazy danych.

--- src/main/java/pl/edu/wszib/jwd/onlinestore/OnlinestoreApplication.java
+++ src/main/java/pl/edu/wszib/jwd/onlinestore/OnlinestoreApplication.java
@@ -2,7 +2,9 @@ package pl.edu.wszib.jwd.onlinestore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:db.properties")
 @SpringBootApplication
 public class OnlinestoreApplication {
 
