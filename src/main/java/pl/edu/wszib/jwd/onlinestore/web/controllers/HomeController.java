9b2b3b4ebbcb3f6030a65d1fc5544c37c2e9d62f
Jacek <jckkupczyk@gmail.com>
2023-07-16 17:38:47 +0200

    Fonts, images, próba controllera homePage.

---
+++ src/main/java/pl/edu/wszib/jwd/onlinestore/web/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package pl.edu.wszib.jwd.onlinestore.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String homePage() {
+
+        return "homePage";
+
+    }
+}
